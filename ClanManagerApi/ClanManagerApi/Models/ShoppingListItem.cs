1c4ecc02e7db123f65abf65b65dee26ab36a6f11
Neal Darley <darley.neal@gmail.com>
2021-11-24 18:55:38 -0700

    Some cleanup, closer to the overall layout

--- ClanManagerApi/ClanManagerApi/Models/ShoppingListItem.cs
+++ ClanManagerApi/ClanManagerApi/Models/ShoppingListItem.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace ClanManagerApi.Models
+﻿namespace ClanManagerApi.Models
 {
     public record ShoppingListItem
     {
---
+++ clan_manager_app/lib/shopping_list_item.dart
@@ -0,0 +1,6 @@
+class ShoppingListItem {
+  String itemName;
+  bool isComplete;
+
+  ShoppingListItem(this.itemName, this.isComplete);
+}
